f24f3363e42a75722b7370ebf0796aa105ac53bd
Ankita Sarkar <ankitasarkar.sectionn@gmail.com>
2026-03-20 21:27:37 +0530

    remove legacy library management classes and add new structure for models and services

--- src/User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    protected int userId;
-    protected String name;
-
-    public User(int userId, String name) {
-        this.userId = userId;
-        this.name = name;
-    }
-}
