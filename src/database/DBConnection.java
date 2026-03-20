19486c6e6fd284f333c348c54ee7f096c802af09
Ankita Sarkar <ankitasarkar.sectionn@gmail.com>
2026-03-20 22:11:58 +0530

    add database connection class for SQLite

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,5 +1,13 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
-  
+
+    private static final String URL = "jdbc:sqlite:library.db";
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL);
+    }
 }
